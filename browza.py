4e9d28d98d2fdb2b00a83f7db9687228cb962d50
Anthony Long <antlong@gmail.com>
2010-07-21 18:04:06 -0400

    Cleaning things up

--- browza.py
+++ browza.py
@@ -1,7 +1,9 @@
 from selenium import selenium
 import unittest, HTMLTestRunner, os, re, sys, time
+suite = unittest.TestSuite()
+loader = unittest.TestLoader()
 
-class TestClass(unittest.TestCase):        
+class TestClass(unittest.TestCase):
     def setUp(self):
         self.verificationErrors = []
         self.selenium = selenium(*self.browser)
@@ -18,39 +20,39 @@ class TestClassCases(TestClass):
 
 
 def set_testcase_browser(obj, browser):
-        try:
-            child_tests = obj._tests
-        except AttributeError:
-            obj.browser = browser
-            return
-        for child in child_tests:
-            set_testcase_browser(child, browser)
-        return browser_list
+    try:
+        child_tests = obj._tests
+    except AttributeError:
+        obj.browser = browser
+        return
+    for child in child_tests:
+        set_testcase_browser(child, browser)
+    return browser_list
+
 
 if __name__ == "__main__":
-    suite = unittest.TestSuite()
-    loader = unittest.TestLoader()
+    print sys.platform
     if sys.platform == 'darwin':
-        browsers = os.listdir('/Users/along/Desktop/Browsers/')
-        browsers.remove(".DS_Store")
         directory = "/Users/along/Desktop/Browsers/"
-        browser_list = []
         local_url = "localhost"
         site_url = "http://www.google.com/"
+        browsers = os.listdir('/Users/along/Desktop/Browsers/')
+        browsers.remove(".DS_Store")
+        print browsers
+        browser_list = []
         for browser in browsers:
             directory_browser = (directory +  browser)
             if re.match("Firefox", browser):
                 browser_list.append(
-                    [local_url, 5555, "*custom %s/Contents/MacOS/firefox-bin" % directory_browser, site_url]
-                    )
+                    [ local_url, 4444, "*custom %s/Contents/MacOS/firefox-bin" % directory_browser, site_url
+                    ])
             elif re.match("Safari", browser):
                 browser_list.append(
-                    [local_url, 5555, "*custom %s/Contents/MacOS/Safari" % directory_browser, site_url]
-                    )
+                [ local_url, 4444, "*custom %s/Contents/MacOS/Safari" % directory_browser, site_url
+                ])
             elif re.match("Chrome", browser):
-                browser_list.append(
-                    [local_url, 5555, "*custom %s/Chrome.app/Contents/MacOS/Chrome" % directory_browser, site_url]
-                    )
+                browser_list.append([local_url, 4444, "*custom %s/Chrome.app/Contents/MacOS/Chrome" % directory_browser, site_url
+                ])
     for browsers in browser_list:
         browser_suite = loader.loadTestsFromTestCase(TestClassCases)
         set_testcase_browser(browser_suite, browsers)
